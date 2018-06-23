cad353c028484070aced6f498a95dfe7382656f0
easyfly007 <easyfly.huang@gmail.com>
2018-06-23 09:30:05 +0800

    update chap03 codes

--- chap03_autoendocder.py
+++ chap03_autoendocder.py
@@ -17,12 +17,19 @@ class AdditiveGaussianNoiseAutoencoder(object):
 		self.n_input = n_input
 		self.n_hidden = n_hidden
 		self.transfer = transfer_function
+		# activation function
 		self.scale = tf.placeholder(tf.float32)
 		self.training_scale = scale
 		network_weights = self._initialize_weights()
 		self.weights = network_weights
 
 		self.x = tf.placeholder(tf.float32, [None, self.n_input])
+		x = self.x + scale*tf.random_normal((n_input,))
+		y = tf.matmul(x, self.weights['w1'])
+		y = tf.add(y, self.weights['b1'])
+		y = self.transfer(y)
+		self.hidden = y
+
 		self.hidden = self.transfer(tf.add(tf.matmul(self.x + scale*tf.random_normal((n_input,)), 
 			self.weights['w1']), self.weights['b1']))
 		self.reconstruction = tf.add(tf.matmul(self.hidden, self.weights['w2']), self.weights['b2'])
